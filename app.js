1aaa0e469a8ed2c79301e13b83d04865d8608ddb
vijayvisaharan <vijayvisaharan@gmail.com>
2024-07-30 22:09:22 +0530

    cors

--- app.js
+++ app.js
@@ -26,10 +26,12 @@ const morgan = require('morgan');
 //use the cors middleware
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'http://localhost:5173', // Replace with your frontend URL
     credentials: true
+  }));
+    
 
-}));
+
 
 //use the cookie parser middleware
 
